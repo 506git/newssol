b79dbb1d35a8f5a0e320fbde20bde0838e215725
506git <85498871+506git@users.noreply.github.com>
2021-10-15 17:24:18 +0900

    timmer 작업중

--- app/src/main/java/com/example/newssolapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newssolapplication/ui/main/MainFragment.kt
@@ -1,40 +1,134 @@
 package com.example.newssolapplication.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.NumberPicker
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.newssolapplication.R
 import com.example.newssolapplication.common.CommonFragment
 import com.example.newssolapplication.databinding.MainFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainFragment : CommonFragment() {
+class MainFragment : CommonFragment(), NumberPicker.OnValueChangeListener {
 
     companion object {
         fun newInstance() = MainFragment()
+        var timer = 0
     }
 
     private lateinit var mainViewModel: MainViewModel
-    private var mBinding: MainFragmentBinding?= null
+    private lateinit var mBinding: MainFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         mainViewModel =
-            ViewModelProvider(this,).get(MainViewModel::class.java).apply {
-//                showProgress()
-            }
+                ViewModelProvider(this).get(MainViewModel::class.java).apply {
+                }
 
-        mBinding = DataBindingUtil.inflate<MainFragmentBinding>(inflater,R.layout.main_fragment, container, false).apply {
+        mBinding = DataBindingUtil.inflate<MainFragmentBinding>(inflater, R.layout.main_fragment, container, false).apply {
             viewModel = mainViewModel
             lifecycleOwner = viewLifecycleOwner
         }
-        return inflater.inflate(R.layout.main_fragment, container, false)
+
+        mBinding.progressTimer.apply {
+            timer = 0
+            max = 780
+        }
+
+        mBinding.timerHour.apply {
+            minValue = 0
+            maxValue = 12
+            wrapSelectorWheel = false
+            value = 0
+            setOnValueChangedListener(this@MainFragment)
+        }
+
+        mBinding.timerMin.apply {
+            minValue = 0
+            maxValue = 59
+            wrapSelectorWheel = false
+            value = 0
+            setOnValueChangedListener(this@MainFragment)
+        }
+
+        var hour = 0
+        var min = 0
+
+        mainViewModel.timeHour.observe(viewLifecycleOwner, Observer {
+            hour = it.toInt()
+        })
+
+        mainViewModel.timeMin.observe(viewLifecycleOwner, Observer {
+            min = it.toInt()
+        })
+
+        mainViewModel.progressTime.observe(viewLifecycleOwner, Observer {
+            timer = it.toInt()
+        })
+
+        mBinding.btnStart.setOnClickListener {
+            mainViewModel.setProgressTimer(hour*60+min)
+            Toast.makeText(context,timer.toString(),Toast.LENGTH_SHORT).show()
+            startTimer()
+        }
+        return mBinding.root
     }
 
+    private fun startTimer() {
+        var jobTime = timer
+        CoroutineScope(IO).launch {
+            do{
+                withContext(Main){
+                    mBinding.progressTimer.smoothProgress(jobTime)
+                    Toast.makeText(context,jobTime.toString(),Toast.LENGTH_SHORT).show()
+                    calTimer(jobTime)
+                }
+                jobTime -= 1
+                delay(1000)
+            }while (jobTime == 0)
+            withContext(Main){
+            Toast.makeText(context,"finish",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
+    private fun calTimer(jobTime: Int) {
+        if (jobTime % 60 == 0){
+            mainViewModel.minusTimeHour()
+        } else {
+            mainViewModel.minusTimeMin()
+        }
+    }
+
+    private fun ProgressBar.smoothProgress(percent: Int){
+        val animation = ObjectAnimator.ofInt(this,"progress",percent)
+        animation.apply {
+            duration = 400
+            interpolator = DecelerateInterpolator()
+            start()
+        }
+    }
+    override fun onValueChange(numberPicker: NumberPicker?, p1: Int, p2: Int) {
+        when (numberPicker?.id){
+            R.id.timer_hour -> {
+                mainViewModel.setTimeHour(p2)
+            }
+            R.id.timer_min -> {
+                mainViewModel.setTimeMin(p2)
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/newssolapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/newssolapplication/ui/main/MainViewModel.kt
@@ -1,7 +1,50 @@
 package com.example.newssolapplication.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val _timeHour = MutableLiveData<Int>()
+    private val _timeMin = MutableLiveData<Int>()
+    private val _progressTimer = MutableLiveData<Int>()
+
+    init {
+        _timeHour.value = 0
+        _timeMin.value = 0
+        _progressTimer.value = 0
+    }
+
+    val timeHour : LiveData<Int>
+        get() = _timeHour
+
+
+    val timeMin : LiveData<Int>
+        get() = _timeMin
+
+    val progressTime : LiveData<Int>
+        get() = _progressTimer
+
+
+    fun setTimeHour(num: Int){
+        _timeHour.value = num
+    }
+
+    fun setTimeMin(num: Int){
+        _timeMin.value = num
+    }
+
+    fun minusTimeHour(){
+        _timeHour.value?.minus(1)
+    }
+
+    fun minusTimeMin(){
+        _timeMin.value?.minus(1)
+    }
+
+    fun setProgressTimer(num: Int){
+        _progressTimer.value = num
+    }
+
 }
